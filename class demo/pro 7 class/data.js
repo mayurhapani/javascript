b91393970594c090da86f3675e0943f912f061fd
mayurhapani <hapanimayur@gmail.com>
2023-12-12 18:47:07 +0530

    login form with class and objects

--- class demo/pro 7 class/data.js
+++ class demo/pro 7 class/data.js
@@ -1,9 +1,32 @@
+let indexNo = -1;
+let gender;
+const nameValidation = document.querySelector("#nameValidation");
+const emailValidation = document.querySelector("#emailValidation");
+const mobNoValidation = document.querySelector("#mobNoValidation");
+const genderValidation = document.querySelector("#genderValidation");
+const courseValidation = document.querySelector("#courseValidation");
+
+let userName;
+let email;
+let mobNo;
+let course;
+
 class user {
   constructor() {
+    this.userData = JSON.parse(localStorage.getItem("userData")) || [];
+  }
+
+  getUserValue() {
+    userName = $("#name").val();
+    email = $("#email").val();
+    mobNo = $("#mobNo").val();
+    course = $("#course").val();
+  }
+
+  show() {
     let output = "";
 
-    userData.forEach((user, index) => {
-      console.log(user.course);
+    this.userData.forEach((user, index) => {
       const userInfo = `<tr>
                   <td>${user.name}</td>
                   <td>${user.email}</td>
@@ -18,13 +41,85 @@ class user {
                 </tr>`;
 
       output += userInfo;
-      localStorage.setItem("userData", JSON.stringify(userData));
+      localStorage.setItem("userData", JSON.stringify(this.userData));
     });
     document.querySelector("#newEntry").innerHTML = output;
   }
-  edit() {}
-  upadte() {}
-  delete() {}
+
+  add() {
+    if (indexNo == -1) {
+      this.userData.push({
+        name: userName,
+        email: email,
+        mobileNo: mobNo,
+        gender: gender,
+        course: course,
+      });
+      alert("Registered Successfull...");
+
+      u1.show();
+    } else {
+      this.userData.forEach((editUser, index) => {
+        if (index === indexNo) {
+          this.userData.splice(index, 1, { name: userName, email: email, mobileNo: mobNo, gender: gender, course: course });
+          u1.show();
+        }
+      });
+      alert("Added Successfull...");
+    }
+  }
+
+  edit(userIndex) {
+    this.userData.forEach((user, index) => {
+      if (userIndex === index) {
+        indexNo = index;
+        document.querySelector("#name").value = user.name;
+        document.querySelector("#email").value = user.email;
+        document.querySelector("#mobNo").value = user.mobileNo;
+        document.querySelector("#course").value = user.course;
+        if (user.gender === "male") {
+          document.querySelector("#gender_male").checked = true;
+        } else document.querySelector("#gender_female").checked = true;
+      }
+    });
+    ragister("login");
+  }
+
+  delete(index) {
+    this.userData.splice(index, 1);
+    u1.show();
+  }
+
+  mainDisplay(page) {
+    if (page === "login") {
+      $("#form").css("display", "block");
+      $("#listData").css("display", "none");
+    } else if (page === "list") {
+      $("#listData").css("display", "block");
+      $("#form").css("display", "none");
+      u1.show();
+    }
+  }
 }
 
 const u1 = new user();
+
+ragister("login");
+
+function ragister(page) {
+  u1.mainDisplay(page);
+}
+function deleteButton(index) {
+  u1.delete(index);
+}
+
+function editButton(userIndex) {
+  u1.edit(userIndex);
+}
+
+function edited(id1, id2) {
+  const inputData = $(id1).val();
+  if (inputData != "") {
+    $(id2).html("<br>");
+  }
+}
--- class demo/pro 7 class/script.js
+++ class demo/pro 7 class/script.js
@@ -1,38 +1,10 @@
-let userData = JSON.parse(localStorage.getItem("userData"));
-let indexNo = -1;
-
-ragister("login");
-
-function ragister(page) {
-  if (page === "login") {
-    $("#form").css("display", "block");
-    $("#listData").css("display", "none");
-  } else if (page === "list") {
-    $("#listData").css("display", "block");
-    $("#form").css("display", "none");
-    u1();
-    // dataDisp();
-  }
-}
-
-const nameValidation = document.querySelector("#nameValidation");
-const emailValidation = document.querySelector("#emailValidation");
-const mobNoValidation = document.querySelector("#mobNoValidation");
-const genderValidation = document.querySelector("#genderValidation");
-const courseValidation = document.querySelector("#courseValidation");
-
-dataDisp();
-
 $("#submit").on("click", () => {
-  const name = $("#name").val();
-  const email = $("#email").val();
-  const mobNo = $("#mobNo").val();
-  const course = $("#course").val();
+  u1.getUserValue();
 
   let nameVarification = (emailVarification = mobNoVarification = genderVarification = courseVarification = true);
 
   // <---------------------------nameValidation---------------------->
-  if (name == "") nameValidation.innerHTML = "Name Cannot Be Blank..!";
+  if (userName == "") nameValidation.innerHTML = "Name Cannot Be Blank..!";
   else {
     nameValidation.innerHTML = "<br>";
     nameVarification = false;
@@ -58,7 +30,6 @@ $("#submit").on("click", () => {
   // <---------------------------genderValidation---------------------->
   const gender_male = document.querySelector("#gender_male");
   const gender_female = document.querySelector("#gender_female");
-  let gender;
 
   if (gender_male.checked) {
     genderValidation.innerHTML = "<br>";
@@ -82,7 +53,7 @@ $("#submit").on("click", () => {
 
   if (nameVarification || emailVarification || mobNoVarification || genderVarification || courseVarification === true) return false;
   else {
-    dataPush();
+    u1.add();
     document.querySelector("#name").value = "";
     document.querySelector("#email").value = "";
     document.querySelector("#mobNo").value = "";
@@ -91,82 +62,4 @@ $("#submit").on("click", () => {
     document.querySelector("#gender_male").checked = false;
     document.querySelector("#gender_female").checked = false;
   }
-
-  // <---------------------------user data push---------------------->
-  function dataPush() {
-    if (indexNo == -1) {
-      userData.push({
-        name: name,
-        email: email,
-        mobileNo: mobNo,
-        gender: gender,
-        course: course,
-      });
-      alert("Login Successfull...");
-
-      dataDisp();
-      u1();
-    } else {
-      userData.forEach((editUser, index) => {
-        if (index === indexNo) {
-          userData.splice(index, 1, { name: name, email: email, mobileNo: mobNo, gender: gender, course: course });
-          // dataDisp();
-          u1();
-        }
-      });
-    }
-  }
 });
-
-// function dataDisp() {
-// let output = "";
-
-// userData.forEach((user, index) => {
-//   console.log(user.course);
-//   const userInfo = `<tr>
-//                 <td>${user.name}</td>
-//                 <td>${user.email}</td>
-//                 <td>${user.mobileNo}</td>
-//                 <td>${user.gender}</td>
-//                 <td>${user.course}</td>
-//                 <td>
-//                   <button onclick="editButton(${index})" class="btn2 edit">Edit</button>
-//                   <button  onclick="deleteButton(${index})"
-//                   class="btn2 delete">Delete</button>
-//                 </td>
-//               </tr>`;
-
-//   output += userInfo;
-//   localStorage.setItem("userData", JSON.stringify(userData));
-// });
-// document.querySelector("#newEntry").innerHTML = output;
-// }
-
-function deleteButton(index) {
-  userData.splice(index, 1);
-  u1();
-  // dataDisp();
-}
-
-function editButton(userIndex) {
-  userData.forEach((user, index) => {
-    if (userIndex === index) {
-      indexNo = index;
-      document.querySelector("#name").value = user.name;
-      document.querySelector("#email").value = user.email;
-      document.querySelector("#mobNo").value = user.mobileNo;
-      document.querySelector("#course").value = user.course;
-      if (user.gender === "male") {
-        document.querySelector("#gender_male").checked = true;
-      } else document.querySelector("#gender_female").checked = true;
-    }
-  });
-  ragister("login");
-}
-
-function edited(id1, id2) {
-  const name = $(id1).val();
-  if (name != "") {
-    $(id2).html("<br>");
-  }
-}
