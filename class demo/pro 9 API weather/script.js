eecf5de3d3bfb09f39b92dacff6b436c90eda356
mayurhapani <hapanimayur@gmail.com>
2023-12-23 21:45:36 +0530

    weather api pro done with function

--- class demo/pro 9 API weather/script.js
+++ class demo/pro 9 API weather/script.js
@@ -1,83 +1,100 @@
 const counSelect = document.querySelector("#allCountriesName");
 const citySelect = document.querySelector("#allCitiesName");
 
-let cityName;
-
-$.ajax({
-  url: `https://countriesnow.space/api/v0.1/countries`,
-  method: "GET",
-  // type: "JSON",
-  async: false,
-
-  success: function (countries) {
-    const coun = countries.data;
-    let country = "";
-    let city = "";
-
-    coun.forEach((coun) => {
-      country += `<option value="${coun.country}">${coun.country}</option>`;
-    });
-    counSelect.innerHTML += country;
-
-    counSelect.addEventListener("change", () => {
-      $("#countryName").html(`${counSelect.value}`);
-      coun.forEach((coun) => {
-        if (coun.country === counSelect.value) {
-          const cities = coun.cities;
-          citySelect.innerHTML = `<option value="" selected disabled>Select City</option>`;
-          city = ``;
-
-          cities.forEach((allCityName) => {
-            city += `<option value="${allCityName}">${allCityName}</option>`;
-          });
-          citySelect.innerHTML += city;
-
-          citySelect.addEventListener("change", () => {
-            cityName = citySelect.value;
-            console.log(cityName);
-            $("#cityName").html(`${cityName}`);
-          });
-        }
-      });
-    });
-  },
-  error: function (error) {
-    $("#cityName").html(error);
-  },
-});
-
-console.log(cityName);
-
-let lat = "21.1418";
-let lon = "72.7709";
 const API = "7bd87f17b32a0f9cda88052b4fdd97bc";
+getCity();
 
-$.ajax({
-  url: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API}&units=metric`,
+function getCity() {
+  let cityName = "";
+  $.ajax({
+    url: `https://countriesnow.space/api/v0.1/countries`,
+    method: "GET",
+    async: false,
 
-  method: "GET",
-  // async: false,
-  success: function (weather) {
-    const sunrise = new Date(`${weather.sys.sunrise}` * 1000).toLocaleString("default", `${weather.timeZone}`);
-    const sunset = new Date(`${weather.sys.sunset}` * 1000).toLocaleString("default", `${weather.timeZone}`);
+    success: function (countries) {
+      const coun = countries.data;
+      let country = "";
+      let city = "";
 
-    // $("#cityName").html(`${weather.name}  ${weather.sys.country}`);
-    $("#temp").html(weather.main.temp);
-    $("#feels_like").html(weather.main.feels_like);
-    $("#humidity").html(weather.main.humidity);
-    $("#pressure").html(weather.main.pressure);
-    $("#windDeg").html(weather.wind.deg);
-    $("#windSpeed").html(weather.wind.speed);
-    // $("#sunrise").html(12 - [weather.sys.sunrise / 15]);
-    $("#sunrise").html(sunrise);
-    // 12 - [ω / 15];
-    // new Date(sunriseUnix * 1000).toLocaleString("default", { timeZone });
-    $("#sunset").html(sunset);
-    // console.log(cityName);
+      coun.forEach((coun) => {
+        country += `<option value="${coun.country}">${coun.country}</option>`;
+      });
+      counSelect.innerHTML += country;
 
-    // console.log(weather);
-  },
-  error: function (error) {
-    $("#cityName").html(error);
-  },
-});
+      counSelect.addEventListener("change", () => {
+        $("#countryName").html(`${counSelect.value}`);
+
+        coun.forEach((coun) => {
+          if (coun.country === counSelect.value) {
+            const countryCoad = coun.iso2;
+            const cities = coun.cities;
+            citySelect.innerHTML = `<option value="" selected disabled>Select City</option>`;
+            city = ``;
+
+            cities.forEach((allCityName) => {
+              city += `<option value="${allCityName}">${allCityName}</option>`;
+            });
+            citySelect.innerHTML += city;
+
+            citySelect.addEventListener("change", () => {
+              cityName = citySelect.value;
+              $("#cityName").html(`${cityName}`);
+
+              getLanLon(cityName, countryCoad);
+            });
+          }
+        });
+      });
+    },
+    error: function (error) {
+      $("#cityName").html(error);
+    },
+  });
+}
+
+function getLanLon(cityName, countryCoad) {
+  country = "in";
+  let lat = "";
+  let lon = "";
+
+  $.ajax({
+    url: `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${countryCoad}&appid=${API}`,
+    method: "GET",
+    async: false,
+
+    success: function (data) {
+      console.log(data);
+      data.forEach((city) => {
+        lat = city.lat;
+        lon = city.lon;
+        getWeatherData(lat, lon);
+      });
+    },
+    error: function (error) {
+      $("#cityName").html(error);
+    },
+  });
+}
+
+function getWeatherData(lat, lon) {
+  $.ajax({
+    url: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API}&units=metric`,
+    method: "GET",
+    async: false,
+    success: function (weather) {
+      const sunrise = new Date(`${weather.sys.sunrise}` * 1000).toLocaleString("default", `${weather.timeZone}`);
+      const sunset = new Date(`${weather.sys.sunset}` * 1000).toLocaleString("default", `${weather.timeZone}`);
+
+      $("#temp").html(weather.main.temp);
+      $("#feels_like").html(weather.main.feels_like);
+      $("#humidity").html(weather.main.humidity);
+      $("#pressure").html(weather.main.pressure);
+      $("#windSpeed").html(weather.wind.speed);
+      $("#sunrise").html(sunrise);
+      $("#sunset").html(sunset);
+    },
+    error: function (error) {
+      $("#cityName").html(error);
+    },
+  });
+}
